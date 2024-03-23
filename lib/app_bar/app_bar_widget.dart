90790db71b2ec1fb73e60c9567398e1d2201277a
crpoudyal <crpoudyal@gmail.com>
2024-03-23 17:14:02 +0545

    updated app bar

--- lib/app_bar/app_bar_widget.dart
+++ lib/app_bar/app_bar_widget.dart
@@ -12,18 +12,20 @@ class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: Colors.white,
-      leadingWidth: 150,
       title: Row(
         children: [
-          const Text('My Shoes. ',
-              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+          const Text('United Shoes. ',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900)),
           Text(slogon,
               style:
                   const TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
         ],
       ),
       elevation: 1,
-      leading: Image.asset(ImageHelper.appIcon),
+      leading: Padding(
+        padding: const EdgeInsets.only(left: 12),
+        child: Image.asset(ImageHelper.appIcon),
+      ),
       shadowColor: Colors.grey,
       actions: [
         Padding(
