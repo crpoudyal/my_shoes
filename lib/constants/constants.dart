41b2343b9d844b806e8793bf8ac5b3d29ebf876e
crpoudyal <crpoudyal@gmail.com>
2024-03-24 15:16:52 +0545

    updated constants

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,6 +1,10 @@
 class Constants {
   static const String appName = 'United Real Leather';
   static const String appSlogon = '100% Pure Leather';
+  static const String madeIn = 'Made in Biratnagar';
+  static const String becomeMember = 'Become a Member';
+  static const String newArrivles = 'New Arrivals';
+  static const String bestSelling = 'Best Selling';
   // contact
   static const String email = 'unitedtanneries@gmail.com';
   static const String phoneNumberMembership = '9820728444 [ Membership ]';
