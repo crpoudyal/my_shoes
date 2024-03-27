2f20e615462aa81f05f71f92de4b95f616a07893
crpoudyal <crpoudyal@gmail.com>
2024-03-27 12:02:49 +0545

    updated delivery screen

--- lib/screens/delivery_screen.dart
+++ lib/screens/delivery_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_shoes/app_bar/app_bar_widget.dart';
+import 'package:my_shoes/constants/color_constants.dart';
 import 'package:my_shoes/constants/constants.dart';
 
 import 'package:timelines/timelines.dart';
@@ -19,6 +20,13 @@ class _DeliveryScreenState extends State<DeliveryScreen> {
     'Reached Our Logistics Facility',
     'Dropped Off',
   ];
+  // List<String> timelineDescription = [
+  //   'Your Package has been delivered',
+  //   'Our delivery partner ARAMEX will attempt to deliver your package today',
+  //   'Your package is on the way to our last mile hub with tracking number ${Constants.trackingNumber} from where it will be delivered to you',
+  //   'Your package has arrived at our logistics facility from where it will be sent to the last mile hub',
+  //   'Your package has been dropped off by the seller and will soon arrive at our logistics facility',
+  // ];
   List<String> timelineTimes = [
     '1:32 PM',
     '11:35 AM',
@@ -45,7 +53,7 @@ class _DeliveryScreenState extends State<DeliveryScreen> {
               height: 100,
               width: double.infinity,
               decoration: BoxDecoration(
-                  color: Colors.deepOrange,
+                  color: ColorConstants.buttomColor,
                   borderRadius: BorderRadius.circular(10)),
               child: const Center(
                   child: Text(
@@ -64,32 +72,38 @@ class _DeliveryScreenState extends State<DeliveryScreen> {
               ),
             ),
             Flexible(
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Timeline.tileBuilder(
-                  builder: TimelineTileBuilder.fromStyle(
-                      connectorStyle: ConnectorStyle.dashedLine,
-                      itemCount: timelineTitle.length,
-                      contentsAlign: ContentsAlign.basic,
-                      contentsBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.all(24.0),
-                          child: Text(
-                            timelineTitle[index],
-                            style: const TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        );
-                      },
-                      oppositeContentsBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.all(24.0),
-                          child: Text(
-                            timelineTimes[index],
-                            style: const TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        );
-                      }),
-                ),
+              child: Timeline.tileBuilder(
+                builder: TimelineTileBuilder.fromStyle(
+                    connectorStyle: ConnectorStyle.dashedLine,
+                    itemCount: timelineTitle.length,
+                    contentsAlign: ContentsAlign.basic,
+                    contentsBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(24.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              timelineTitle[index],
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            // Text(
+                            //   timelineDescription[index],
+                            // ),
+                          ],
+                        ),
+                      );
+                    },
+                    oppositeContentsBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(24.0),
+                        child: Text(
+                          timelineTimes[index],
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      );
+                    }),
               ),
             ),
           ],
