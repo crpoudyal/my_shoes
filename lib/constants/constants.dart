9a75dbc95b2d138b62d0f91dc02323e648dffc61
crpoudyal <crpoudyal@gmail.com>
2024-03-28 10:37:06 +0545

    updated constants and images

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -4,16 +4,16 @@ class Constants {
   static const String madeIn = 'Made in Biratnagar';
   static const String becomeMember = 'Become a Member';
   static const String newArrivles = 'New Arrivals';
-  static const String bestSelling = 'Best Selling';
+  static const String bestSellers = 'Bestsellers';
   static const String shoesMarketplace = 'Shoes Marketplace';
-  static const String madeinnepal = 'Made in Nepal';
+  static const String genuineLeather = '100% Genuine Leather';
   static const String availableDaraz = 'Available in daraz';
   static const String paymentPartner = 'Payment Partner';
   // contact
-  static const String email = 'unitedtanneries@gmail.com';
+  static const String email = 'sales@unitedtanneries.com.np';
   static const String phoneNumberMembership = '9820728444';
   static const String phoneNumberBusiness = '9820728444';
-  static const String webSite = 'www.daraz.com.np/shop/united-tanneries/';
+  static const String webSite = 'www.unitedtanneries.com.np';
   static const String googleMapLink =
       "https://www.google.com/maps/place/PRACAS+Infosys/@26.4542582,87.2765091,17z/data=!3m1!4b1!4m6!3m5!1s0x39ef74473b03b5e1:0xd7abc8d2f474745e!8m2!3d26.4542534!4d87.279084!16s%2Fg%2F1q6jkbjxg?entry=ttu";
   static const String pracasInfosys = 'https://pracas.net/';
@@ -26,9 +26,6 @@ class Constants {
   // delivery
   static const String deliveryPartnerName = 'Aramex';
   static const String trackingNumber = 'ARAMEX7987878978787';
-
-  // search
-  static const String searchCategories = 'Search by categories';
 
   // shoes details
   static const String buyNow = 'Buy Now';
